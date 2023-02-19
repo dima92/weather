b4379b086e3cb0099c45d60047ed9353cfbfe59f
dima92 <dima-tkachenko@mail.ru>
2023-02-19 03:39:18 +0300

    Завершение приложения

--- weather.js
+++ weather.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
-import { getWeather } from "./services/api.service.js";
-import { printHelp, printSuccess, printError } from "./services/log.service.js";
-import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
+import { getWeather, getIcon } from "./services/api.service.js";
+import {
+  printHelp,
+  printSuccess,
+  printError,
+  printWeather,
+} from "./services/log.service.js";
+import {
+  saveKeyValue,
+  TOKEN_DICTIONARY,
+  getKeyValue,
+} from "./services/storage.service.js";
 
 const saveToken = async (token) => {
   if (!token.length) {
@@ -17,10 +26,24 @@ const saveToken = async (token) => {
   }
 };
 
-const getForecast = async () => {
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError("Не передан город");
+    return;
+  }
   try {
-    const weather = await getWeather(process.env.CITY);
-    console.log(weather); // Красивый вывод погоды
+    await saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess("Город сохранён");
+  } catch (e) {
+    printError(e.message);
+  }
+};
+
+const getForcast = async () => {
+  try {
+    const city = process.env.CITY ?? (await getKeyValue(TOKEN_DICTIONARY.city));
+    const weather = await getWeather(city);
+    printWeather(weather, getIcon(weather.weather[0].icon));
   } catch (e) {
     if (e?.response?.status == 404) {
       printError("Неверно указан город");
@@ -35,14 +58,15 @@ const getForecast = async () => {
 const initCLI = () => {
   const args = getArgs(process.argv);
   if (args.h) {
-    printHelp();
+    return printHelp();
   }
   if (args.s) {
+    return saveCity(args.s);
   }
   if (args.t) {
     return saveToken(args.t);
   }
-  getForecast();
+  return getForcast();
 };
 
 initCLI();
